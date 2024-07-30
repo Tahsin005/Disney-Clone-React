bbd5d59a36138928a0246d779db8dda3b41ed986
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-30 09:39:39 +0600

    added the header/navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-8xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header></Header>
     </>
   )
 }
